22d25d89ab9362dd5cf41e2d7be9af7a6d0597a1
Arandor <33958575+Flabulous@users.noreply.github.com>
2020-11-04 11:12:40 -0500

    created a release

--- goof.h
+++ goof.h
@@ -1,25 +1,26 @@
 #include "SDL.h"
-//Main data
-FILE *rom_file;
-struct sbank {unsigned char *bank;};
-struct sbank rom[256];
-unsigned char *ram;
-unsigned char a,b,c,d; //Registers: a: Accumulator | b: Logical Accumulator | c: RAM pointer | d: ROM pointer
-
-//Back end data:
-int initTable();
-void (*inst_table[256])();
-int GUI_ACTIVE = 0;
 
 //Front end data:
 int cpuRuntime();
 int pcopenrom();
 int pcsetuprom();
 
+//Back end data:
+int initTable();
+void (*inst_table[256])();
+int GUI_ACTIVE;
+
 //GUI data:
 int initGUI();
 int refreshGUI();
 SDL_Event e;
+
+//CPU data
+FILE *rom_file;
+struct sbank {unsigned char *bank;};
+struct sbank rom[256];
+unsigned char *ram;
+unsigned char a,b,c,d; //Registers: a: Accumulator | b: Logical Accumulator | c: RAM pointer | d: ROM pointer
 
 //Instructions:
 //...
